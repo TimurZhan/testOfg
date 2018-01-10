e7d15021304ee83854ff2d51826024fd67d39938
Timur <zhanchikov@ofd.ru>
2018-01-10 18:47:24 +0300

    Создан Front-168

---
+++ src/test/java/ru/stqa/pft/testOfg/tests/documentsTabLK/Front168HelpOldUser.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.testOfg.tests.documentsTabLK;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.testOfg.tests.TestBase;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+//Front-168:Новые подсказки в разделе документы (старый пользователь)
+public class Front168HelpOldUser extends TestBase {
+
+  @Test
+  public void testHelpOldUser () throws InterruptedException, IOException, SQLException {
+    String email = "yaschenko@ofd.ru";
+    String password = "121288";
+
+    app.getNavigationHelper().loginToSystem(email, password);
+    app.getNavigationHelper().checkelpOldUser();
+    app.getNavigationHelper().signOut();
+
+  }
+
+}
